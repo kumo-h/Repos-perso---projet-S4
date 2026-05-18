76fa5dec5fa39e3e8769e2da17c5f3229290805c
Inès <inesdhommeelebouc@gmail.com>
2026-05-18 18:08:10 +0200

    Mise à jour du nom de repo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Repos-perso---projet-S4/'
+  base: '/Projet_S4_BrainApp/'
 })
